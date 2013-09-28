2693f34e8f1df48e87ea192a5ae810aeccd125af
Ben Eills <ben@beneills.com>
2013-09-28 12:36:31 +0100

    Move entry creation code to daemon

--- app/models/top_entry.rb
+++ app/models/top_entry.rb
@@ -1,17 +1,6 @@
 class TopEntry < ActiveRecord::Base
   belongs_to :user
 
-  def self.higher_level_create(hash)
-    # get user id, possibly creating new user
-    username = hash.delete(:username)
-    u = User.find_by_username(username)
-    if u.nil?
-      u = User.new(username: username)
-      return(false) if !u.save
-    end
-    u.entries.create(hash)
-  end
-
   def to_s # TODO
     @text
   end
