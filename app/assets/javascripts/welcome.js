175f836bcf93a724ff5a3dff3b25a7b1afcfe644
Ben Eills <ben@beneills.com>
2013-10-02 14:17:56 +0100

    Hide help cookie

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,6 +3,12 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $( document ).ready( function () {
+    if (!$.cookie('hide_help') || $.cookie('hide_help') == "false") {
+	$( '#help' ).show();
+    } else {
+	$( "#help-show" ).show();
+    }
+
 //    $( "#tweet-body" ).keypress(function() {
     $( "#tweet-button" ).click(function() {
 	var tweet = $( "#tweet-body" ).val();
@@ -14,6 +20,7 @@ $( document ).ready( function () {
     });
 
     $( "#help-show" ).click(function() {
+	$.cookie('hide_help', 'false', { expires: 365*100 });
 	// http://stackoverflow.com/questions/5003220/javascript-jquery-animate-to-auto-height
 	var help = $('#help'),
 	helpHeight = help.height();
@@ -26,6 +33,7 @@ $( document ).ready( function () {
 
 
     $( "#help-dismiss" ).click(function() {
+	$.cookie('hide_help', 'true', { expires: 365*100 });
 	$('#help').animate({height: "10px"}, 600, function () {
 	    $('#help').fadeOut("fast", function (){
 		$( "#help-show" ).fadeIn();
