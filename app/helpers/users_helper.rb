81f59f0968427ff128d674130fddf9dcd5829863
Ben Eills <ben@beneills.com>
2013-10-08 18:05:01 +0100

    Cite styling/help

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
 end
+
+module Enumerable
+  def each_or_message(message=nil, &block)
+    if empty?
+      message || "no entries yet"
+    else
+      each(&block)
+      nil
+    end
+  end
+end
--- app/models/cite_entry.rb
+++ app/models/cite_entry.rb
@@ -1,7 +1,28 @@
 class CiteEntry < ActiveRecord::Base
   belongs_to :user
 
+  @@response_regexp = /(?<url>http:\/\/www.webcitation.org\/[^"]+)">\(Archived by WebCite/i
+
+
   def archived?
     !archive_url.nil?
+  end
+
+  # delayed_job
+  def fetch_archive_url
+    #logger.info("Performing citation via WebCite")
+    escaped = URI.escape(url)
+    target = "http://www.webcitation.org/archive?url=#{escaped}&email=webcite@hashcatch.com"
+    # HTTP GET
+    response = open(target).read
+    #logger.debug("Got HTTP response")
+    m = @@response_regexp.match(response)
+
+    if m
+      #logger.debug("Response correct.  Updating archive_url.")
+      update(archive_url: m[:url])
+    else
+      #logger.error("WebCite request unsuccessful")
+    end
   end
 end
